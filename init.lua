bac219fd712967cf65c57b7f422817a0a0e8ee1f
--global <--global>
2026-01-04 07:12:52 +0100

    additional lua love2d support

--- init.lua
+++ init.lua
@@ -314,7 +314,17 @@ require('lazy').setup({
       },
     },
   },
-
+{
+  "S1M0N38/love2d.nvim",
+  event = "VeryLazy",
+  version = "2.*",
+  opts = { },
+  keys = {
+    { "<leader>v", ft = "lua", desc = "LÖVE" },
+    { "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+    { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+  },
+},
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
